1a3c7dd28b3399feb8aeb79cfafedb05a005ad21
alessiomongoli <64589166+alessiomongoli@users.noreply.github.com>
2021-09-06 11:06:06 +0200

    Add files via upload

---
+++ stop_words.py
@@ -0,0 +1,87 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+
+STOP_WORDS = set(
+    """
+a  abbia abbiamo abbiano abbiate accidenti accidente ad adesso affinche agl
+agli ahime ahimè ai al alcun alcuna alcuni alcuno all alla alle allo allora altri altre
+ altro  altrui anche ancora ancorare andare anni anno ansa ansare anticipo anticipare  attraversare
+ attraverso avanti avemmo avendo avente aver avere averlo avesse
+avessero avessi avessimo aveste avesti avete aveva avevamo avevano avevate
+avevi avevo avrai avranno avrebbe avrebbero avrei avremmo avremo avreste
+avresti avrete avrà avrò avuta avute avuti avuto
+
+basta bastare  brava bravo
+
+casa caso cento certa certe certi certo che chi chicchessia chiunque ci ciascun
+ciascuna ciascuno cima cimare  cio cioe cioè circa citta città ciò co codesta codesti codesto
+cogli coi col colei coll colorire coloro colui come cominci cominciare comprare comunque con concernente concernere
+conciliarsi conclusione consecutivi
+consecutivo  contro  cos cosa cosi così cui
+
+da dagl dagli dai dal dall dalla dalle dallo dappertutto dare  davanti degl degli
+dei del dell della delle dello dentro detto deve devo di dio dice dietro dire diro
+dirimpetto diventa diventare diventato dopo dov dovere dove dovra dovrà  due
+dunque durante durare
+
+e ebbe ebbero ebbi ecc ecco ed effettivamente egli ella entrambi  era
+erano eravamo eravate eri ero esempio esse essendo esser essere essi ex
+
+fa faccia facciamo facciano facciate faccio facemmo facendo facesse facessero
+facessi facessimo faceste facesti faceva facevamo facevano facevate facevi
+facevo fai fanno farai faranno fare farebbe farebbero farei faremmo faremo
+fareste faresti farete farà farò fatto favore fece fecero feci fin finalmente
+finche fine fino forse forza forzare fossa fosso fosse fossero fossi fossimo foste fosti fra
+frattempo fu fui fummo fuori furono  generale
+
+giacca governare generale gente gia già giacche giorni giorno giu già giã gli gliela gliele glieli glielo gliene governo
+ gruppo
+
+ha haha hai hanno ho
+
+ieri il improvviso improvvisare in  infatti inoltre insieme intanto intorno  io
+
+la là lasciare lasciato lato lavorare lavoro le lei li lo loro lui lungo luogo
+
+ma macche  maggior mai  malgrado marca marche me
+medesimo mediare mediante   mentre mese mesi mezzo mi mia mie miei mila miliardi miliardo
+milione milioni minimi ministrare ministro mio modo   momento mondare mondo mosto
+
+nazionale ne negl negli nei nel nell nella nelle nello  no noi nome  nondimeno  nonsia nostra nostre
+nostri nostro novanta nove  nuovi nuovo
+
+od oggi ogni ognuna ognuno oltre oppure ora orare ore osare osi ossia ottanta otto
+
+paese parecchi parecchie parecchio parte partendo partire per perche
+perché percio perciò perfino pero persino persona persone però piede piedi pieno piglia pigliare piu
+piuttosto piã¹ più po   poi poiche possa possedere postare posteriore posto potere
+potrebbe preferibilmente presa preso press prima primo principalmente probabilmente promesso promettere
+proprio puo può pure 
+
+qua qualche qualcosa qualcuna qualcuno quale quali qualunque quando quanta quante
+quanti quanto quantunque quasi quattro quel quella quelle quelli quello quest
+questa queste questi questo qui quindi
+
+realmente recente recentemente registrazione relativo riecco  salvo
+
+sara sarà sarai saranno sarebbe sarebbero sarei saremmo saremo sareste
+saresti sarete saro sarò scola scolare scopare scopo scorrere scorso se secondare secondo seguente seguire seguitare seguito sei
+sembra sembrare sembrato sembri sempre setta sette si sia siamo siano siate
+siete sig solere solito solo soltanto sono sopra sotto spesso srl sta stai stando
+stanno starai staranno stare starebbe starebbero starei staremmo staremo stareste
+staresti starete starà starò stata state stati stato stava stavamo stavano
+stavate stavi stavo stemmo stessa stesse stessero stessi stessimo stesso
+steste stesti stette stettero stetti stia stiamo stiano stiate sto su sua subire
+subito successivamente successivo sue sugl sugli sui sul sull sulla sulle
+sullo suo suoi
+
+tale tali talvolta tangere tanto te tempo ti titolare titolo torino tra  tre trenta
+trovare trovato tu tua tue tuo tuoi tutta tuttavia tutte tutti tutto
+
+uguale uguali ulteriore ultimare  ultimo un una uno uomo
+
+va vale valere vari varia variare varie vario varo versare verso vi via visitare visto vita voi volta volte volto
+vostra vostre vostri vostro
+""".split()
+)
